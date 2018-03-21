ca3eb65764f66d1dc714c14c4f8486a2a12ba2ab
vks4git <vks_m@niuitmo.ru>
2018-03-21 15:36:00 +0300

    New instances.

--- src/Database/Bolt/Id.hs
+++ src/Database/Bolt/Id.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric   #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Database.Bolt.Id
@@ -6,14 +7,21 @@ module Database.Bolt.Id
   , fromInt
   ) where
 
+import           Control.DeepSeq            (NFData (..))
+import           Data.Aeson                 (FromJSON (..), ToJSON (..))
 import           Database.Bolt              (Node (..), Relationship (..),
                                              URelationship (..))
 import           Database.Bolt.Extras.Utils (currentLoc)
-import Debug.Trace
+import           Debug.Trace
+import           GHC.Generics               (Generic (..))
 
 -- | 'BoltId' is wrapper for Bolt 'Node', 'Relationship' and 'URelationship' identities.
 newtype BoltId = BoltId { boltId :: Int }
-  deriving (Show, Read, Eq, Ord)
+  deriving (Show, Read, Eq, Ord, Generic)
+
+instance NFData BoltId
+instance ToJSON BoltId
+instance FromJSON BoltId
 
 fromInt :: Int -> BoltId
 fromInt i | i >= 0    = BoltId i
