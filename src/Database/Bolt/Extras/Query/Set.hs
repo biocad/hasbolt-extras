75545f5f32749be810626de1340a6a4882eaa2d4
Cheshirrrrrr <kochkova@rain.ifmo.ru>
2018-04-23 16:13:39 +0300

    version 0.0.0.7: deleteNodes

--- src/Database/Bolt/Extras/Query/Set.hs
+++ src/Database/Bolt/Extras/Query/Set.hs
@@ -10,7 +10,7 @@ import           Control.Monad.IO.Class              (MonadIO)
 import           Data.Text                           (Text, append, intercalate)
 import           Database.Bolt                       (BoltActionT,
                                                       RecordValue (..), at,
-                                                      exact, query)
+                                                      exact, query, Value (..))
 import           Database.Bolt.Extras.Persisted      (BoltId, fromInt)
 import           Database.Bolt.Extras.Query.Cypher   (ToCypher (..))
 import           Database.Bolt.Extras.Query.Get      (NodeGetter, condIdAsText,
@@ -26,11 +26,11 @@ setNode nodeGetter props = do
     let nodeGetterT = nodeAsText (varQ, nodeGetter)
     let condId      = condIdAsText (varQ, nodeGetter)
 
-    let newProperties = intercalate ", " $ fmap formPropertySet props
+    let newProperties = intercalate "," $ fmap formPropertySet $ filter ((/= N ()) . snd) props
 
     let getQuery = [text|MATCH $nodeGetterT
                          WHERE $condId
-    					           SET $newProperties
+                         SET $newProperties
                          RETURN ID($varQ) as $varQ|]
 
     record <- head <$> query getQuery
