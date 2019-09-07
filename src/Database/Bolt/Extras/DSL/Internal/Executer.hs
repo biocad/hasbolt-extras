8f9aefe24c50ce58b36cb1728f0d13a22b754563
Maxim Koltsov <kolmax94@gmail.com>
2019-09-07 19:56:02 +0300

    version 0.0.0.20: add WITH statement (#28)

--- src/Database/Bolt/Extras/DSL/Internal/Executer.hs
+++ src/Database/Bolt/Extras/DSL/Internal/Executer.hs
@@ -30,6 +30,7 @@ execute (Delete t n)        = executeHelperT "DELETE " t n
 execute (DetachDelete t n)  = executeHelperT "DETACH DELETE " t n
 execute (Remove t n)        = executeHelperT "REMOVE " t n
 execute (Return t n)        = executeHelperT "RETURN " t n
+execute (With t n)          = executeHelperT "WITH " t n
 execute (Text t n)          = tell [t] >> pure n
 
 -- | Helper to translate 'Expr' with something, which can be translated to cypher.
