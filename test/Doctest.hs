e3cba16ec34c2e1cd49d418a04e5c300ccb06b2a
Maxim Koltsov <kolmax94@gmail.com>
2019-10-25 13:35:54 +0300

    Add doctest to cabal

---
+++ test/Doctest.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import           Test.DocTest (doctest)
+
+-- Taken from https://github.com/kowainik/membrain/blob/master/test/Doctest.hs
+
+main :: IO ()
+main =
+  doctest
+    [ "-isrc"
+    , "src/Database/Bolt/Extras/DSL/Typed.hs"
+    ]
