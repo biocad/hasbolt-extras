a7d7ef685e5773a68da860587ef3a094186d219f
Maxim Koltsov <kolmax94@gmail.com>
2021-09-07 16:34:05 +0300

    version 0.0.1.8: NonEmpty instances, BoltEnum (#45)
    * Add ToValue / FromValue instances for NonEmpty
    
    * Add BoltEnum DerivingVia wrapper

--- test/Doctest.hs
+++ test/Doctest.hs
@@ -11,6 +11,7 @@ main = do
     , "src/Database/Bolt/Extras/DSL/Typed.hs"
     , "src/Database/Bolt/Extras/DSL/Typed/Types.hs"
     , "src/Database/Bolt/Extras/DSL/Typed/Parameters.hs"
+    , "src/Database/Bolt/Extras/Generic.hs"
     ]
   --  This has to be run separately due to some complications with TH and/or internal modules
   --  See here: https://github.com/sol/doctest/issues/160
